36ea743c13ba56f7658b8a3d35fa898958a69665
ConhecedorX~ <Lucastation2@gmail.com>
2025-01-28 16:24:57 -0300

    Começo do Projeto

---
+++ conhece-cooks/src/main/java/com/example/conhece_cooks/ConheceCooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.conhece_cooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConheceCooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConheceCooksApplication.class, args);
+	}
+
+}
---
+++ conhece-cooks/src/test/java/com/example/conhece_cooks/ConheceCooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conhece_cooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConheceCooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
